ac4dd04b786c55d2bf8e9a269f8f07a2162cee6a
VPAbraham <vpa456@gmail.com>
2019-09-11 17:39:35 -0600

    Import jQuery UI and create tab structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,62 @@
 // This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you import jQuery into a JS file if you use jQuery in that file
 import $ from 'jquery';
-
-// An example of how you tell webpack to use a CSS (SCSS) file
+import 'jquery-ui-bundle';
 import './css/base.scss';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// <-- FETCH -->
 
-console.log('This is the JavaScript entry file - your code begins here.');
+let users, rooms, bookings, roomServices;
+
+fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users")
+  .then(data => data.json())
+  .then(data => users = data.users)
+  .catch(err => console.log('Unable to fetch data', err));
+
+fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms")
+  .then(data => data.json())
+  .then(data => rooms = data.rooms)
+  .catch(err => console.log('Unable to fetch data', err));
+
+fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings")
+  .then(data => data.json())
+  .then(data => bookings = data.bookings)
+  .catch(err => console.log('Unable to fetch data', err));
+
+fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/room-services/roomServices")
+  .then(data => data.json())
+  .then(data => roomServices = data.roomServices)
+  .catch(err => console.log('Unable to fetch data', err));
+
+  setTimeout(() => console.log(users, rooms, bookings, roomServices), 3000);
+
+// <--  -->
+
+$(document).ready(function() {
+  $('#ui-tabs').tabs();
+  // $('#select-menu').selectmenu();
+})
+
+// let $tabButtons = $('.tab-container, .tab-btn-container .main-btn .orders-btn .rooms-btn .customer-btn');
+// let $tabPanels = $('.tab-container, .tab-panel')
+
+// function showPanel(panelIndex, colorCode) {
+//   $tabButtons.forEach((node) => {
+//     node.style.backgroundColor='';
+//     node.style.color=''
+//   });
+//   $tabButtons[panelIndex].style.backgroundColor=colorCode;
+//   $tabButtons[panelIndex].style.color = 'white';
+//   $tabButtons.forEach((node) => {
+//     node.style.display='none';
+//   });
+//   $tabPanels[panelIndex].style.display = 'block';
+//   $tabPanels[panelIndex].style.backgroundColor = colorCode;
+// } 
+// function meh() {
+//   console.log('x');
+// }
+
+// $('.main-btn').click(meh());
+// $('.orders-btn').click(console.log('y'));
+// $('.rooms-btn').click(showPanel(2, 'pink'));
+// $('.customer-btn').click(showPanel(3, 'purple'));
