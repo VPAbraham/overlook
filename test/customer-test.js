98e0c6ee6b81f81751b8c0e86f9b4c849f5f7806
VPAbraham <vpa456@gmail.com>
2019-09-18 04:51:34 -0600

    Continue file cleanup and smal refactors

--- test/customer-test.js
+++ test/customer-test.js
@@ -12,15 +12,21 @@ describe('Customer', () => {
     hotel.hotelPrepHandler()
   });
 
-  it('should be a function that will instatiate customers', () => {
+  it('should be a function that will instantiate customers', () => {
     expect(Customer).to.be.a('function');
     expect(hotel.customers[10]).to.be.an.instanceof(Customer);
   });
 
-
-  it('should be a function that will instatiate customers', () => {
-    expect(hotel.customers[0].getCurrentBooking()).to.deep.equal(1);
+  it('should be able to store its\' own bookings', () => {
+    expect(hotel.customers[4].bookings.shift().userID).to.equal(5);
   });
 
+  it('should be able to find its\' room services on a given date', () => {
+    expect (hotel.customers[12].getRoomServicesByDate('2019/10/01').shift().food).to.equal('Handcrafted Metal Sandwich');
+  })
+
+  it('should be able to find all of it\'s room services', () => {
+    expect(hotel.customers[12].getAllRoomServices().length).to.equal(2);
+  })
 
 });
